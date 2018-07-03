e4d3cc76b230a472db38b6d5a4b10077ad043749
Mark Musante <mark.musante@gmail.com>
2018-07-03 18:42:52 -0400

    attempting to load from a specific filename

--- src/nautsTeamRandomizer/Tests/TestLoadingAndSaving.java
+++ src/nautsTeamRandomizer/Tests/TestLoadingAndSaving.java
@@ -3,14 +3,27 @@ package nautsTeamRandomizer.Tests;
 import org.junit.jupiter.api.Test;
 
 import nautsTeamRandomizer.TeamRandomizerController;
+import nautsTeamRandomizer.Model.AwesomenautsPlayer;
 
 class TestLoadingAndSaving {
 
 	@Test
 	void test() {
-		TeamRandomizerController trc = new TeamRandomizerController();
+		TeamRandomizerController trc1 = new TeamRandomizerController();
 		
-		trc.save("my_data");
+		AwesomenautsPlayer ap = new AwesomenautsPlayer("jerry");
+		boolean b[] = {true, false, true, false, false, false, false, false, true, false, false, false,
+				false, false, false, false, false, false, false, false, false, false, false, false, false,
+				false, false, false, false, false, false, false, true, false};
+		ap.setHasNauts(b);
+		
+		trc1.addNewPlayer(ap);
+		trc1.save("my_data");
+		
+		TeamRandomizerController trc2 = new TeamRandomizerController();
+		
+		trc2.load("my_data");
+
 	}
 
 }
